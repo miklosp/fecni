f6b619fb35c0fcd5df88f109a0d2c981dc8570b3
Miklos <miklos.petravich@gmail.com>
2026-05-28 20:49:29 +0200

    Initial Commit

---
+++ fecni/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fecni
+//
+//  Created by Miklos Petravich on 5/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fecni/fecniApp.swift
@@ -0,0 +1,17 @@
+//
+//  fecniApp.swift
+//  fecni
+//
+//  Created by Miklos Petravich on 5/28/26.
+//
+
+import SwiftUI
+
+@main
+struct fecniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
